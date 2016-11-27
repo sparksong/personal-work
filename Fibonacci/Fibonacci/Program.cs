0b7736425bc3ae5096f98b7a1447cc791b141084
Justin Frederiksen <Justin.Frederiksen@gmail.com>
2016-11-26 21:59:49 -0600

    Fibonacci

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many numbers of the fibonacci sequence would you like to go?");
+
+            double n = int.Parse(Console.ReadLine());
+
+            double a = 0;
+            double b = 1;
+
+            for (double i = 0; i < n; i++)
+            {
+                double temp = a;
+                a = b;
+                b = temp + b;
+                Console.WriteLine(a);
+            }
+            Console.ReadKey();
+        }
+    }
+}
